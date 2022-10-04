c09951b5a976e9a71af48f9f834d3c0589371d9e
DipShtick <vancouver_cliparf@simplelogin.com>
2022-10-04 18:03:38 +0100

    0.0.2

--- Assets/Scripts/Weapon/Pistol.cs
+++ Assets/Scripts/Weapon/Pistol.cs
@@ -1,38 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Pistol : MonoBehaviour
 {
-    public Weapon value;
     [SerializeField] private Transform firepoint;
     [SerializeField] private GameObject bullet;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Searches for corresponding needs
         bullet = GameObject.Find("pis_bullet");
+        cam = Camera.main;
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    public Player user;
-
-    private void shoot()
-    {
-        if (user)
-        {
-
-        }
-    }
-
+    public Camera cam;
+    Vector3 mousePos;
     // Update is called once per frame
     void Update()
+    {
+        mousePos = Mouse.current.position.ReadValue();
+        Vector3 lookDir = mousePos - transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        rb.rotation = angle;
+        rb.position = Vector2.right;
+    }
+    
+    private InputAction look;
+    public Rigidbody2D rb;
+
+    // Changes rotation according to the mouse position
+    void FixedUpdate()
     {
         
     }
 
-    private void lookatMouse()
+    // Input script
+    /*Controls mouse;
+    private void Awake()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.up = (mousePos - new Vector2(transform.position.x - transform.position.y));
+        mouse = new Controls();
     }
+    private void OnEnable()
+    {
+        look.Enable();
+    }
+
+    private void OnDisable()
+    {
+        look.Disable();
+    }  
+    */
 }
